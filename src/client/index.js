36014bb73ba40c739367768e46132da789936328
Surya Kant Bansal <suryakantbansal97@gmail.com>
2019-08-18 23:18:59 +0530

    complete client application

---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+import { handleSubmit, handleSubmitArticle } from "./js/interactions.js";
+
+import "./styles/base.scss";
+import "./styles/header.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/main.scss";
+
+export { handleSubmit, handleSubmitArticle };
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devServer: {
+    host: "localhost",
+    port: "3000",
+    hot: true,
+    overlay: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    historyApiFallback: true
+  },
+  entry: "./src/client/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.min.js",
+    libraryTarget: "var",
+    library: "Client"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      hash: true,
+      xhtml: true
+    })
+  ]
+};
